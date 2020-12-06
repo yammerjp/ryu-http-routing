a9a8159080aecbe12b1720a1e8dbf219370067a9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-12-07 04:00:51 +0900

    Update init.sh for vagrant

--- init.sh
+++ init.sh
@@ -1,27 +1,26 @@
 #!/bin/bash -e
 
 # ubuntu:20.04
-sed -i.bak -e "s%http://archive.ubuntu.com/ubuntu/%http://ftp.jaist.ac.jp/pub/Linux/ubuntu/%g" /etc/apt/sources.list
-apt-get update \
-apt-get dist-upgrade -y \
-apt-get install -y sudo git make gnupg2 python3 python3-pip net-tools\
-rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get dist-upgrade -y
+apt-get upgrade -y
+apt-get install -y sudo git make gnupg2 python3 python3-pip net-tools
 
 # install mininet
-git clone https://github.com/basd4g/mininet.git /root/mininet \
-   && cd /root/mininet \
-   && apt-get update \
-   && ./util/install.sh -nfv \
-   && rm -rf /var/lib/apt/lists/*
+cd
+git clone https://github.com/basd4g/mininet.git
+cd mininet
+./util/install.sh -nfv
 
+cd
 apt-get remove -y openvswitch-common openvswitch-switch openvswitch-pki openvswitch-testcontroller
-git clone https://github.com/basd4g/ovs.git /root/ovs \
-   && cd /root/ovs \
-   && ./boot.sh \
-   && ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc \
-   && make \
-   && make install\
+git clone https://github.com/basd4g/ovs.git
+cd ovs
+./boot.sh
+./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc
+make
+make install
 
-git clone https://github.com/basd4g/ryu.git /root/ryu \
-   && cd /root/ryu \
-   && pip3 install .
+git clone https://github.com/basd4g/ryu.git
+cd ryu
+pip3 install .
